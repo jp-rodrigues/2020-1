569cf7e0e4612378bc928b80630e5748577c31d4
jp-rodrigues <rjoaopaulo066@gmail.com>
2020-09-10 21:58:05 -0300

    continuando a responder

---
+++ exercicio_1/q1.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+echo -e "voce é capaz do que quiser, menos ganhar do flamengo se for o vasco!"
---
+++ exercicio_1/q2.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+
+read -p "digite o nome do primeiro diretorio a ser listado" dir1
+
+read -p "digite o nome do segundo diretorio a ser listado" dir2
+
+read -p "digite o nome do terceiro diretorio a ser listado" dir3
+
+
+echo -e "$(ls -l ${dir1})"
+
+echo -e "$(ls -l ${dir2})"
+
+echo -e "$(ls -l ${dir3})"
+
