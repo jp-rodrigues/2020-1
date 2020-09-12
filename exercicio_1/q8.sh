6ad8440aeb08fdbed8f10236493e8ef6e71d41b0
jp-rodrigues <rjoaopaulo066@gmail.com>
2020-09-12 18:08:55 -0300

    finalizado

---
+++ exercicio_1/q8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'subistituição de variavel se utiliza ${variavel}, exemplo ${a},a informação da variavel é substiuida no local indicado pelo campo ${}
+
+na subistituição de shell se utiliza $(comando), exemplo $(cat arquivo),na subtituição de shell aconte amesma coisa só que sera o resultado de um comando que sera substituido '
