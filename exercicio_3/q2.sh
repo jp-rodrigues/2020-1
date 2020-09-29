c41c3fcb6a94a0eee15471214a04fef37398a0e4
jp-rodrigues <rjoaopaulo066@gmail.com>
2020-09-29 12:20:50 -0300

    resolução da q2.sh

---
+++ exercicio_3/q2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "digite o nome do primeiro arquivo" n1
+
+read -p "segundo nome" n2
+
+read -p "terceiro nome" n3
+
+l1=$(cat ${n1} | wc -l)
+l2=$(cat ${n2} | wc -l)
+l3=$(cat ${n3} | wc -l)
+
+((l1 > l2)) && ((l1 > l3)) && echo "${n1}"
+((l2 > l1)) && ((l2 > l3)) && echo "${n2}"
+((l3 > l1)) && ((l3 > l2)) && echo "${n3}"
+
+
+
+
