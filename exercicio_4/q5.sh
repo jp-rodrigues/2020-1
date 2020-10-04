c54abe493f6745c8c430c556f1bac4d1eaf1a884
jp-rodrigues <rjoaopaulo066@gmail.com>
2020-10-03 23:32:44 -0300

    continuando

--- exercicio_4/q5.sh
+++ exercicio_4/q5.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-while true; do
-	a=$(( ${RANDOM} % 10 ))
-	touch ${a} && echo “$(date +%H:%M) ${a}: criado!” >> 1.log 2>> 2.log
-	sleep 1
-b=$(( ${RANDOM} % 10 ))
-	rm ${b} && echo “$(date +%H:%M) ${b}: removido!” >>1.log 2>> 2.log
-done
 
-while true; do
-	a=$(( ${RANDOM} % 10 ))
-	touch ${a} && echo “$(date +%H:%M) ${a}: criado!” &>> 3.log
-	sleep 1
-b=$(( ${RANDOM} % 10 ))
-	rm ${b} && echo “$(date +%H:%M) ${b}: removido!” &>> 3.log
-done
