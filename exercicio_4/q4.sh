e3b30b8e8efd48c0e73e72d5fea09dbb053bcfa2
jp-rodrigues <69464669+jp-rodrigues@users.noreply.github.com>
2020-10-03 22:52:01 -0300

    Update q4.sh

--- exercicio_4/q4.sh
+++ exercicio_4/q4.sh
@@ -3,9 +3,9 @@ workdir=”/tmp/workdir”
 mkdir $workdir
 cd $workdir 2> /dev/null
 ls $1 2>/dev/null || echo “Por favor crie o arquivo ${workdir}/${1}” 2>/dev/null
-ls $1 2>/dev/null || exit 1 2>/dev/null
+ls $1 2>/dev/null || exit 1 
 ls $2 2>/dev/null || echo “Por favor crie o arquivo ${workdir}/${2}” 2>/dev/null
-ls $2 2>/dev/null || exit 1 2>/dev/null
+ls $2 2>/dev/null || exit 1
 ls $3 2>/dev/null || echo “Por favor crie o arquivo ${workdir}/${3}” 2>/deev/null
-ls $3 2>/dev/null || exit 1 2>/dev/null
+ls $3 2>/dev/null || exit 1 
 cat $1 $2 $3 2>/dev/null | tr ‘ ‘ ‘\n’ 2>/dev/null | sort 2>/dev/null | grep -v “comentario” 2>/dev/null
