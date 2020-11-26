44316fb524523d35779b5cf11cc71abfff18467e
jp-rodrigues <69464669+jp-rodrigues@users.noreply.github.com>
2020-11-26 18:42:37 -0300

    Update q6.sh

--- exercicio_11/q6.sh
+++ exercicio_11/q6.sh
@@ -1,1 +1,18 @@
+{
+	acc=0
+	for( i=1; i<=NF; i++ )
+	{
+		if( $i ~ /[0-9]+/ ) acc+=$i;
+	}
+
+	if( acc/3.0 >= 70 )
+	{
+		
+    for( a=1; a<=NF; a++ )
+		{
+			if( $a ~ /[^0-9]+/ ) printf $a" ";
+		}
+		printf "\n";
+	}
+}
 
