9966d940ec3409d1e0afc93e6a19ca662f767df1
jp-rodrigues <rjoaopaulo066@gmail.com>
2020-12-08 13:40:11 -0300

    resolução da lita 14

---
+++ exercicio_14/q1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "esse é um novo dia de vitória, $(date)"
+#o script estando dentro de /etc/init.d/meu_script
+#cloquei o caminho do script dentro de /home/usuario/.profile
---
+++ exercicio_14/q2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv /home/$(whoami)/tmp /home 
+
+#cloquei o script dentro do diretorio /etc/init.d/meu_script
+# e executei update-rc.d meu_script defaults
