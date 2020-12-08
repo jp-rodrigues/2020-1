afbec439531476a99caf65d32708d6993f46e2fd
jp-rodrigues <rjoaopaulo066@gmail.com>
2020-12-08 10:48:29 -0300

    update da q2 da lista 13

--- exercicio_13/q2.sh
+++ exercicio_13/q2.sh
@@ -2,15 +2,30 @@
 
 . q1.sh
 #ordenei o aruivo primeiro
-declare -A v
 
-for (( i=0; i< $(wc -l < orde); i++ ));do 
-	for x in $(cat orde);do
-		v[i]=$x
-	done
+
+v=($(awk -F " " '{print $1}' orde))
+
+
+
+valor_maior=${v[0]}
+valor_menor=${v[0]}
+
+for (( i=0; i <= ${#v[*]}; i++ ));do
+	if (( $(maior ${v[i]} ${v[i+1]}) > $valor_maior ));then
+		valor_maior=$(maior ${v[i]} ${v[i+1]}) 
+			
+	fi
+
 done
 
+for (( i=0; i <= ${#v[*]}; i++ ));do
+	if (( $(menor ${v[i]} ${v[i+1]}) < $valor_menor ));then
+		valor_menor=$(menor ${v[i]} ${v[i+1]}) 
 
-
-echo $(maior )
-echo $(menor )
+	fi		
+done	
+		
+		
+echo $valor_maior
+echo $valor_menor
